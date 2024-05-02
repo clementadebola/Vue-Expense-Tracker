00052da6f127d8bee221bb62e3cadbf2b28dcef9
clement <clementadebola54@gmail.com>
2024-05-03 00:24:34 +0100

    new commmit

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,14 +1,23 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <!-- <li class="minus">
+        <li v-for="transaction in transactions" :key="transaction.id">
             cash <span>-$400</span>
             <button class="delete-btn">x</button>
         </li>
-
-        <li class="plus">
-                Paycheck <span>+$800</span>
-            <button class="delete-btn">x</button>
-        </li> -->
+       
     </ul>
 </template>
+
+<script>
+export default{
+    data(){
+        transactions: [
+            {id:1, text: 'flower', amount: -19.99},
+            {id:2, text: 'salary', amount: 299.99},
+            {id:3, text: 'Book', amount: -10},
+            {id:4, text: 'Camera', amount: 150},
+        ]
+    }
+}
+</script>
